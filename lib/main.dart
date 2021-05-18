eeca77add05c1ea556efe8f1d4894855b8578f25
96Daniel96 <the.daniel.basilio@gmail.com>
2021-05-18 23:10:19 +0100

    Stateful component sample

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var counter = 0;
+
+  void next() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     /** Scaffold creates a default page that most apps use, very similar to Paper */
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text('My First App')),
-            body: Text('Default Data')));
+            body: Column(children: [
+              Text(
+                'Counter: ' + counter.toString(),
+              ),
+              ElevatedButton(child: Text('Save'), onPressed: next)
+            ])));
   }
 }
